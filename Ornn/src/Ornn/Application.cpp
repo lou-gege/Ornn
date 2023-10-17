4540507b240822aa573a7961f0bf5b23cd2451ce
gege <lougege@hotmail.com>
2023-10-17 22:38:05 +0800

    Add Event System

--- Ornn/src/Ornn/Application.cpp
+++ Ornn/src/Ornn/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Ornn/Events/ApplicationEvent.h"
+#include "Ornn/Log.h"
+
 namespace Ornn {
 
 	Application::Application()
@@ -10,6 +13,16 @@ namespace Ornn {
 	}
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			ORNN_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			ORNN_TRACE(e);
+		}
+
 		while (true)
 		{
 
