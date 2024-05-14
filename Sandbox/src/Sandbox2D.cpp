ea212672ca5b1575f44e3ad466e99048dae7307c
gege <lougege@hotmail.com>
2024-05-14 22:22:13 +0800

    Add Renderer2D

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -13,26 +13,6 @@ Sandbox2D::Sandbox2D()
 
 void Sandbox2D::OnAttach()
 {
-	m_SquareVA = Ornn::VertexArray::Create();
-
-	float squareVertices[5 * 4] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.5f,  0.5f, 0.0f,
-		-0.5f,  0.5f, 0.0f
-	};
-
-	auto squareVB = Ornn::VertexBuffer::Create(squareVertices, sizeof(squareVertices));
-	squareVB->SetLayout({
-		{ Ornn::ShaderDataType::Float3, "a_Position" }
-	});
-	m_SquareVA->AddVertexBuffer(squareVB);
-
-	uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
-	auto squareIB = Ornn::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t));
-	m_SquareVA->SetIndexBuffer(squareIB);
-
-	m_FlatColorShader = Ornn::Shader::Create("assets/shaders/FlatColor.glsl");
 }
 
 void Sandbox2D::OnDetach()
@@ -48,14 +28,13 @@ void Sandbox2D::OnUpdate(Ornn::Timestep ts)
 	Ornn::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 	Ornn::RenderCommand::Clear();
 
-	Ornn::Renderer::BeginScene(m_CameraController.GetCamera());
+	Ornn::Renderer2D::BeginScene(m_CameraController.GetCamera());
+	Ornn::Renderer2D::DrawQuad({ 0.0f,0.0f }, { 1.0f,1.0f }, { 0.8f,0.2f,0.3f,1.0f });
+	Ornn::Renderer2D::EndScene();
 
-	std::dynamic_pointer_cast<Ornn::OpenGLShader>(m_FlatColorShader)->Bind();
-	std::dynamic_pointer_cast<Ornn::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
-
-	Ornn::Renderer::Submit(m_FlatColorShader, m_SquareVA);
-
-	Ornn::Renderer::EndScene();
+	// TODO: Add these functions - Shader::SetMat4, Shader::SetFloat4
+	// std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader)->Bind();
+	// std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
 }
 
 void Sandbox2D::OnImGuiRender()
