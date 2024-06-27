c9ede9cf6d3a1886e2574d71c89b9964554be9e9
gege <lougege@hotmail.com>
2024-06-27 22:44:26 +0800

    Add quad batch

--- Ornn/src/Platform/OpenGL/OpenGLRendererAPI.h
+++ Ornn/src/Platform/OpenGL/OpenGLRendererAPI.h
@@ -14,6 +14,6 @@ namespace Ornn {
 		virtual void SetClearColor(const glm::vec4& color) override;
 		virtual void Clear() override;
 
-		virtual void DrawIndexed(const std::shared_ptr<VertexArray>& vertexArray) override;
+		virtual void DrawIndexed(const std::shared_ptr<VertexArray>& vertexArray, uint32_t indexCount = 0) override;
 	};
 }
